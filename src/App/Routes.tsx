8252afce85ff200c93f319b5c2240876d1aba173
marc-antoine ourradour <ourradourm@gmail.com>
2023-04-30 11:49:21 +0200

    adding test route

--- src/App/Routes.tsx
+++ src/App/Routes.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes as AppRoutes, Route } from "react-router-dom";
 import SubmitProposal from "../pages/SubmitProposal/SubmitProposal";
 import Proposals from "../pages/Proposals/Proposals";
+import TemplatePage from "../pages/TemplatePage/TemplatePage";
 
 const Routes = () => {
   return (
@@ -9,6 +10,7 @@ const Routes = () => {
       <Route path="/" element={<Proposals />} />
       <Route path="/proposals" element={<Proposals />} />
       <Route path="/submitProposal" element={<SubmitProposal />} />
+      <Route path="/test" element={<TemplatePage />} />
     </AppRoutes>
   );
 };
